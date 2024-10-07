8642afeb5305e6a501449108918975d11e89e1ae
DariaBerezina <ladydashaberezina@mail.ru>
2024-10-08 00:42:29 +0600

    Add FINALNEWLINE to Files

--- spacebattle/SpaceBattle.Lib.Tests/obj/Debug/net6.0/SpaceBattle.Lib.Tests.AssemblyInfo.cs
+++ spacebattle/SpaceBattle.Lib.Tests/obj/Debug/net6.0/SpaceBattle.Lib.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceBattle.Lib.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b13c4341f2c8a8960e5864f269f08c5dd223da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be0c9e0b70974908fa4b1d98e395ed2c778c012")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceBattle.Lib.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceBattle.Lib.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
